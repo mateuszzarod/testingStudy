6e5eb7ba253261ea34354a732fe4156c8f49cdd2
mateuszzarod <mateusz.zarod@gmail.com>
2020-05-07 14:25:14 +0200

    Commit 2020

--- kodilla-patterns/src/main/java/com/kodilla/patterns/OOP/Main.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/OOP/Main.java
@@ -2,11 +2,12 @@ package com.kodilla.patterns.OOP;
 
 public class Main {
     public static void main(String[] args) {
-        Shape circle = new Circle();
-        ShapeDrawer drawer = new ShapeDrawer(circle);
-        drawer.process();
+
+        Shape circle1 = new Circle();
+        ShapeDrawer draw = new ShapeDrawer(circle1);
+        draw.process();
 
         //defalut method
-        circle.drawBig();
+        circle1.drawBig();
     }
 }
