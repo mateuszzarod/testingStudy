173f51b28e198004a8d07be5cf618d55f3d135a8
mateuszzarod <mateusz.zarod@gmail.com>
2020-05-07 23:58:23 +0200

    Commit 0520

--- kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -8,6 +8,9 @@ public class Calculator {
     public int remove(int a, int b){
         return a-b;
     }
+    public int multiply(int a, int b){
+        return a*b;
+    }
 
     public int subtract(int a, int b){
         return a-b;
--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -1,19 +1,8 @@
 package com.kodilla.testing.collection;
+
 import java.util.ArrayList;
 
 public class OddNumbersExterminator {
-
-    public ArrayList<Integer> removeExterminate(ArrayList<Integer> numbers) {
-        ArrayList<Integer> evenNumbers = new ArrayList<>();
-
-        for (int i = 0; i < numbers.size(); i++) {
-            int number = numbers.get(i);
-            if (number % 2 == 0) {
-                evenNumbers.add(number);
-            }
-        }
-        return evenNumbers;
-    }
 
     public ArrayList<Integer> removeExterminate2(ArrayList<Integer> numbers) {
         ArrayList<Integer> evenNumbers = new ArrayList<>();
@@ -25,7 +14,17 @@ public class OddNumbersExterminator {
             }
         }
         return evenNumbers;
+    }
 
+    public ArrayList<Integer> removeExterminate(ArrayList<Integer> numbers) {
+        ArrayList<Integer> evenNumbers = new ArrayList<>();
 
+        for (int i = 0; i < numbers.size(); i++) {
+            int number = numbers.get(i);
+            if (number % 2 == 0) {
+                evenNumbers.add(number);
+            }
+        }
+        return evenNumbers;
     }
 }
