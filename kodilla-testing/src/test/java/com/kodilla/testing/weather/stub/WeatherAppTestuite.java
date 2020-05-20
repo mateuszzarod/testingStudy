c06c3e62c8a8679558f32a4b1ff75479bafbb29c
mateuszzarod <mateusz.zarod@gmail.com>
2020-05-20 14:59:56 +0200

    TDD Biblioteka

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/weather/stub/WeatherAppTestuite.java
@@ -0,0 +1,35 @@
+package com.kodilla.testing.weather.stub;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class WeatherAppTestuite {
+    @Test
+    public void testCalculateForecastWithMock() {
+        //Given
+        Temperatures temperaturesMock = mock(Temperatures.class);
+        HashMap<Integer, Double> temperaturesMap = new HashMap<Integer, Double>();
+        HashMap<Integer, Double> temperaturesMap2 = new HashMap<Integer, Double>();
+        temperaturesMap.put(0, 25.5);
+        temperaturesMap.put(1, 26.2);
+        temperaturesMap.put(2, 24.8);
+        temperaturesMap.put(3, 25.2);
+        temperaturesMap.put(4, 26.1);
+        when(temperaturesMock.getTemperatures()).thenReturn(temperaturesMap);
+
+        WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
+
+        //When
+        int quantityOfSensors = weatherForecast.calculateForecast().size();
+
+        //Then
+        Assert.assertEquals(5, quantityOfSensors);
+    }
+
+
+}
